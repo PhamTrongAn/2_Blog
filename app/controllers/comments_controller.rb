947c1e9c61aa2b37c864e9e0a410683867ca1842
An Pham <phamtrongan@outlook.com>
2018-06-29 10:01:47 +0000

    add comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  # POST /comments
+  # POST /comments.json
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:name, :body))
+
+    redirect_to post_path(@post)
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+end
