5c66ab2cf6154ceda7abb85e5c0dee6b58165078
An Pham <phamtrongan@outlook.com>
2018-06-29 10:19:53 +0000

    add pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   end
 
   root to: "posts#index"
+
+  get '/about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
